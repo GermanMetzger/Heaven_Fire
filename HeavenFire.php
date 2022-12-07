b3c689d474f5721d3348cfddba0ce2ba77115c11
GermanMetzger <metzgergerman@gmail.com>
2022-12-07 04:44:44 -0300

    principio

--- HeavenFire.php
+++ HeavenFire.php
@@ -2,13 +2,186 @@
 include_once("funciones.php");
 //Este es mi primer juego, mi idea seria hacer un juego de aventura rpg con historia y que obtenja un puntaje de xp final para almacenar por jugador, y que tenga varios finales. 
 
-// VARIABLES IMPORTANTES
-$hp = 100;
-$monedero = 0;
+// ARREGLOS
+$estado = [
+"hpMax" => 100,
+"hp" => 100,
+"monedero" => 0,
+"velocidad" => 5,
+"daño" => 3,
+"inventario" => 7,
+"xp" => 0,
+"vidas" => 1
+] ;
+
+$inventario = [] ;
 
 
+//variables
+$espaciosOcupados = 0;
 
 
 //               PROGRAMA PRINCIPAL              \\
+
+echo escribirGris("              ****Bienvenido a HEAVEN FIRE****               ");
+echo "\n\n\n";
+do{
+    echo "ingresa el nombre de tu HEROE:";
+    $heroe = trim(fgets(STDIN));
+    espacios();
+    echo "Estas seguro de que el nombre de tu personaje es: ";
+    echo escribiramarillo($heroe) ."?\n";
+    echo "1) ES EL MEJOR NOMBRE DE LA VIDA!!\n";
+    echo "2) mmmm creo que no.\n";
+    $eleccion = solicitarNumeroEntre(1,2);
+    espacios();
+    switch($eleccion){
+        case 1:
+            echo "Perfecto, te llamaremos: ";
+            echo escribirVerde($heroe)."\n";
+            
+            break;
+        case 2:
+            echo "Si, ese era horrible\n";
+            break;
+    }
+}while($eleccion == 2);
+espacios();
+echo "Preparete por que esta aventura esta por comenzar....\nSiempre que quieras consultar los comandos puedes utilizar";
+echo escribiramarillo("/help")."\n\n";
+echo escribirVerde("Antes de comenzar puedes elegir algun atributo u objeto de inicio para tu aventura. (presiona ENTER al ver este simbolo: »)\n");
+trim(fgets(STDIN));
+
+echo "    \n";
+echo escribirRojo("Amuleto de Arkarot");
+echo "
+     /\
+   .'  `.
+ .'      `.
+ `.      .'
+   `.  .'
+     \/
+";
+echo "DESCRIPCION: El amuleto de Arkarot fue encontrado debajo de una laguna perdida en las fronteras de \n";
+echo escribirGris("Divinia").". Este amuleto aumenta tu ♥salud maxima♥ (100 -> 125) »»";
+trim(fgets(STDIN));
+espacios();
+echo "    \n";
+echo escribirRojo("Meliniano");
+echo "
+ ,-,-. 
+/ (_o \ 
+\ o ) / 
+ `-'-' 
+";
+echo "DESCRIPCION: Fuiste parte de un clan a las mitades de una jungla oculta.\n Tu ♦ velocidad ♦ aumenta de (5 -> 11) »» ";
+trim(fgets(STDIN));
+espacios();
+echo "    \n";
+echo escribirRojo("Bolsa");
+echo "
+       .--._ .
+        \ ).'
+         )|/
+      _.'''-._
+     (        \
+      \        )
+      )'-.    (
+     /     _.-'\
+    /           )
+   ('-._       /
+    \        _/ 
+     '-.__=='' 
+";
+echo "DESCRIPCION: Es una simple bolsa.\n Tu ◄inventario► aumenta de (7 -> 10) »» ";
+trim(fgets(STDIN));
+espacios();
+echo "    \n";
+echo escribirRojo("Piedra");
+echo "
+   /---\
+  /     \
+  \     /
+   \---/  
+";
+echo "DESCRIPCION: es una piedra, de algo servira...\nSe agregara una piedra en tu inventario. »» ";
+trim(fgets(STDIN));
+do{
+    echo "1)";
+    echo escribirRojo("Amuleto de Arkarot")."\n";
+    echo "2)";
+    echo escribirRojo("Meliniano")."\n";
+    echo "3)";
+    echo escribirRojo("Bolsa")."\n";
+    echo "4)";
+    echo escribirRojo("Piedra")."\n";
+    echo "5)";
+    echo escribirRojo("Nada")."\n";
+    $eleccion = solicitarNumeroEntre(1,5);
+    espacios();
+    switch($eleccion){
+        case 1:
+            echo "Agregar amuleto de Arkarot?\n";
+            echo "1)Si.\n";
+            echo "2)No.\n";
+            $bandera = solicitarNumeroEntre(1,2);
+            if ($bandera == 1){
+                $arkarot = true;
+                $inventario["slot".$espaciosOcupados]["arkarot"] = $arkarot;
+                $espaciosOcupados++;
+                echo "Amuleto de Arkarot agregado.\n";
+                echo "tu ♥salud maxima♥ aumenta de (100 -> 125).\n";
+                $estado["hpMax"] = $estado["hpMax"] + 25;
+
+            }
+            break;
+        case 2:
+            echo "Ser Meliliano?\n";
+            echo "1)Si.\n";
+            echo "2)No.\n";
+            $bandera = solicitarNumeroEntre(1,2);
+            if ($bandera == 1){
+                $estado["velocidad"] = $estado["velocidad"] + 6;
+                echo "Tu ♦ velocidad ♦ aumenta de (5 -> 11).\n";
+            }
+            
+            break;
+        case 3:
+            echo "Agregar Bolsa?\n";
+            echo "1)Si.\n";
+            echo "2)No.\n";
+            $bandera = solicitarNumeroEntre(1,2);
+            if ($bandera == 1){
+                $estado["inventario"] = $estado["inventario"] + 3;
+                echo "Tu ◄inventario► aumenta de (7 -> 10).\n";
+            }
+
+            break;
+        case 4:
+            echo "Agregar Piedra?\n";
+            echo "1)Si.\n";
+            echo "2)No.\n";
+            $bandera = solicitarNumeroEntre(1,2);
+            if ($bandera == 1){
+                $piedra = true;
+                $inventario["slot".$espaciosOcupados]["piedra"] = $piedra;
+                $espaciosOcupados++;
+                echo "Piedra agregada.\n";
+            }
+            
+            break;
+        case 5:
+            echo "Seguro que no quieras nada??\n";
+            echo "1)Si.\n";
+            echo "2)No.\n";
+            $bandera = solicitarNumeroEntre(1,2);
+            if ($bandera == 1){
+                Echo "O sos muy valiente o sos estupido. »»";
+                trim(fgets(STDIN));
+            }
+    }
+}while($bandera == 2);
+print_r($inventario);
+print_r($estado);
 
 
--- funciones.php
+++ funciones.php
@@ -2,28 +2,115 @@
 
 //El proposito de este archivo es almacenar funciones que deba reutilizar muchas veces.
 
-//Muestra en pantalla la cantidad de $$$DINERO$$$ que tiene.
-function mostrarMonedero($dinero){
-    echo "$".$dinero."\n";
-}
 
-//Muestra en pantalla la cantidad de ♥♥♥SALUD♥♥♥ que tiene.
-function mostrarSalud($salud){
-    echo "♥".$salud."\n";
-}
+
+
 
 //Muestra en pantalla el estado del jugador.
-function status($dinero,$salud){
-    echo "♥".$salud;
-    echo "$".$dinero."\n";
+function status($vida,$salud,$dinero,$velocidad,$daño,$espacio,$xp){
+    echo "↨vidas↨ ->  ".$vida."\n";
+    echo "♥Salud♥ -> ".$salud."\n";
+    echo '$Monedas$ -> '.$dinero."\n";
+    echo "♦Velocidad♦ -> ".$velocidad."\n";
+    echo "¤Daño¤ -> ".$daño."\n";
+    echo "◄Espacio► ->  ".$espacio."\n";
+    echo "§Experiencia§ ->  ".$xp."\n";
 }
+
 
 //Muestra todos los comando posibles en ese momento.
 function help(){
     echo "/help: ver todos los comandos.\n";
-    echo "/status: ver todos el estado del personaje.\n";
-    echo "/salud ver la salud del personaje.\n";
-    echo "/monedero ver la cantidad de dinero que tiene tu personaje.\n";
-    
-
+    echo "/estado: ver todos el estado del personaje.\n";
+    echo "/inventario: ver ti inventario y objetos.\n";
+    echo "/nivel: te muestra cuanto te falta para subir de nivel.\n";
+    echo "/atributos: enseña los atributos adquiridos.\n";
+    echo "/suicidarme: tu personaje se suicida y acaba la partida por completo.\n";
 }
+
+
+/**
+ * Esta funcion pide un numero y verifica que este dentro de los parametros formales.
+ * @param int $min 
+ * @param int $max
+ * @return int $numero
+ */ 
+function solicitarNumeroEntre($min, $max)
+{
+    //int $numero
+    $numero = trim(fgets(STDIN));
+	
+	if (is_numeric($numero)) { //determina si un string es un número. puede ser float como entero.
+        $numero  = $numero * 1; //con esta operación convierto el string en número.
+    }
+	
+    while (!is_numeric($numero) ||( is_int($numero) && !($numero >= $min && $numero <= $max))) {
+        echo "Debe ingresar un número entre " . $min . " y " . $max . ": ";
+        $numero = trim(fgets(STDIN));
+		if (is_numeric($numero)) {
+			$numero  = $numero * 1; 
+		}
+    }
+    
+    return $numero;
+}
+
+
+
+//salta 3 lineas del codigo.
+function espacios(){
+    echo "\n";
+    echo "\n";
+    echo "\n";
+}
+
+
+/**
+ * Escrbir un texto en color ROJO
+ * @param string $texto)
+ */
+function escribirRojo($texto)
+{
+    echo "\e[1;37;41m $texto \e[0m";
+}
+
+
+/**
+ * Escrbir un texto en color VERDE
+ * @param string $texto)
+ */
+function escribirVerde($texto)
+{
+    echo "\e[1;37;42m $texto \e[0m";
+}
+
+
+/**
+ * Escrbir un texto en color AMARILLO
+ * @param string $texto)
+ */
+function escribirAmarillo($texto)
+{
+    echo "\e[1;37;43m $texto \e[0m";
+}
+
+
+/**
+ * Escrbir un texto en color GRIS
+ * @param string $texto)
+ */
+function escribirGris($texto)
+{
+    echo "\e[1;34;47m $texto \e[0m";
+}
+
+
+/**
+ * Escrbir un texto pantalla.
+ * @param string $texto)
+ */
+function escribirNormal($texto)
+{
+    echo "\e[0m $texto \e[0m";
+}
+
